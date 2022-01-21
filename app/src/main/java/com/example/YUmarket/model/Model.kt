eb1f42e3a2af87eebdb39e8732c230daa743e20b
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-01-21 16:05:36 +0900

    0.5.0 Model에 두 Item이 같은지 판단하는 Method 추가 Model의 DIFF_CALLBACK에 areItemsTheSame이 Model의 isTheSame Method를 이용하여 두 Item이 같은지 판단하도록 수정

--- app/src/main/java/com/example/YUmarket/model/Model.kt
+++ app/src/main/java/com/example/YUmarket/model/Model.kt
@@ -7,10 +7,21 @@ abstract class Model(
     open val id : Long,
     open val type : CellType
 ) {
+
+    /**
+     * 두 Item이 같은지 판단하는 Method
+     * @author Main 정남진, Sub 김건우, 김도엽, 배은호, 허희태
+     * @since 2022.01.21
+     * @param item 비교할 Item
+     * @return 두 Item이 같으면 True, 다르면 False
+     */
+    open fun isTheSame(item: Model) =
+        this.id == item.id && this.type == item.type
+
     companion object {
         val DIFF_CALLBACK = object: DiffUtil.ItemCallback<Model>() {
             override fun areItemsTheSame(oldItem: Model, newItem: Model): Boolean {
-                return oldItem.id == newItem.id && oldItem.type == newItem.type
+                return oldItem.isTheSame(newItem)
             }
 
             @SuppressLint("DiffUtilEquals")
