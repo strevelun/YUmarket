f198504d78fe963b21b6f948c4242fde8d07e169
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-01-21 16:13:13 +0900

    0.5.0 ViewHolderMapper에서 추가된 CellType을 when문에 추가하여 알맞은 ViewHolder를 생성할 수 있도록 수정

--- app/src/main/java/com/example/YUmarket/util/mapper/ViewHolderMapper.kt
+++ app/src/main/java/com/example/YUmarket/util/mapper/ViewHolderMapper.kt
@@ -1,6 +1,5 @@
 package com.example.YUmarket.util.mapper
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.YUmarket.databinding.ViewholderHomeItemBinding
@@ -8,13 +7,10 @@ import com.example.YUmarket.databinding.ViewholderNearbyMarketBinding
 import com.example.YUmarket.databinding.ViewholderTownMarketBinding
 import com.example.YUmarket.model.CellType
 import com.example.YUmarket.model.Model
-import com.example.YUmarket.model.homelist.HomeListModel
-import com.example.YUmarket.model.homelist.category.HomeListCategory
 import com.example.YUmarket.screen.base.BaseViewModel
 import com.example.YUmarket.util.provider.ResourcesProvider
 import com.example.YUmarket.widget.adapter.viewholder.ModelViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.home.HomeItemModelViewHolder
-import com.example.YUmarket.widget.adapter.viewholder.home.HomeModelViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.home.TownMarketViewHolder
 import com.example.YUmarket.widget.adapter.viewholder.homemain.NearbyMarketViewHolder
 
@@ -37,33 +33,23 @@ object ViewHolderMapper {
                     resourcesProvider
                 )
             }
+
+            CellType.HOME_TOWN_MARKET_CELL -> {
+                TownMarketViewHolder(
+                    ViewholderTownMarketBinding.inflate(inflater),
+                    viewModel,
+                    resourcesProvider
+                )
+            }
+
+            CellType.HOME_ITEM_CELL -> {
+                HomeItemModelViewHolder(
+                    ViewholderHomeItemBinding.inflate(inflater),
+                    viewModel,
+                    resourcesProvider
+                )
+            }
         } as ModelViewHolder<M>
 
-    }
-
-    @SuppressLint("UNCHECKED_CAST")
-    fun <M: HomeListModel> map(
-        parent: ViewGroup,
-        category: HomeListCategory,
-        viewModel: BaseViewModel,
-        resourcesProvider: ResourcesProvider
-    ): HomeModelViewHolder<M> {
-        val inflater = LayoutInflater.from(parent.context)
-
-        val viewHolder = when(category) {
-            HomeListCategory.TOWN_MARKET -> TownMarketViewHolder(
-                ViewholderTownMarketBinding.inflate(inflater),
-                viewModel,
-                resourcesProvider
-            )
-
-            else -> HomeItemModelViewHolder(
-                ViewholderHomeItemBinding.inflate(inflater),
-                viewModel,
-                resourcesProvider
-            )
-        }
-
-        return viewHolder as HomeModelViewHolder<M>
     }
 }
