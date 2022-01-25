92862387066d6be60526e39d23e90457123f77cb
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2022-01-25 17:00:51 +0900

    0.7.0 새로운 할인 상품의 정보를 저장할 MutableLiveData와 LiveData 정의(_itemData, itemData) 모든 할인 상품의 정보를 저장할 List 정의(allNewSaleItemsData)

--- app/src/main/java/com/example/YUmarket/screen/home/homemain/HomeMainViewModel.kt
+++ app/src/main/java/com/example/YUmarket/screen/home/homemain/HomeMainViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.YUmarket.data.repository.restaurant.HomeRepository
 import com.example.YUmarket.model.CellType
+import com.example.YUmarket.model.homelist.HomeItemModel
+import com.example.YUmarket.model.homelist.category.HomeListCategory
 import com.example.YUmarket.screen.base.BaseViewModel
 import com.example.YUmarket.util.LocationData
 import com.example.YUmarket.util.LocationState
@@ -26,28 +28,60 @@ class HomeMainViewModel(
     private val _marketData = MutableLiveData<HomeMainState>(HomeMainState.Uninitialized)
     val marketData: LiveData<HomeMainState> = _marketData
 
+    private val _itemData = MutableLiveData<HomeMainState>(HomeMainState.Uninitialized)
+    val itemData: LiveData<HomeMainState> = _itemData
+
+    private lateinit var allNewSaleItemsList: List<HomeItemModel>
+
     override fun fetchData(): Job = viewModelScope.launch {
         // get list after get location data
+        if (LocationData.locationStateLiveData.value is LocationState.Success) {
+            fetchMarketData()
+            fetchItemData()
+        }
+    }
+
+    private suspend fun fetchMarketData() {
         // 22.01.19 성공적으로 불러온 뒤에는 reloadData 이외에 Data는 계속 불러오는 것을 방지
         // by 정남진
-        if (marketData.value !is HomeMainState.Success &&
-            LocationData.locationStateLiveData.value is LocationState.Success
+        if (marketData.value !is HomeMainState.Success<*>
         ) {
             _marketData.value = HomeMainState.Loading
 
             // sorted by distance
             _marketData.value = HomeMainState.Success(
                 // 임시로 CellType을 ViewModel에서 변경
-                marketModelList = homeRepository.getAllMarketList().map {
+                modelList = homeRepository.getAllMarketList().map {
                     it.copy(type = CellType.HOME_CELL)
                 }.sortedBy { it.distance }
             )
         }
+    }
 
+    private suspend fun fetchItemData() {
+        if (itemData.value !is HomeMainState.Success<*>
+        ) {
+            _itemData.value = HomeMainState.Loading
+
+            allNewSaleItemsList = homeRepository.getAllNewSaleItems()
+            _itemData.value = HomeMainState.ListLoaded
+        }
     }
 
     fun reloadData(): Job {
         _marketData.value = HomeMainState.Loading
+        _itemData.value = HomeMainState.Loading
         return fetchData()
+    }
+
+    fun setItemFilter(category: HomeListCategory) {
+        if (::allNewSaleItemsList.isInitialized) {
+            _itemData.value = HomeMainState.Success(
+                // 임시로 CellType을 ViewModel에서 변경
+                modelList = allNewSaleItemsList.filter {
+                    it.homeListCategory == category
+                }
+            )
+        }
     }
 }
